6c2e6c54e68cb1b3bac48688c33d80915211597b
Clockwork EJD <4930532+ClockworkEJD@users.noreply.github.com>
2022-07-07 15:48:14 -0400

    Update wordcount_cmd.rb

--- plugins/custom/wordcount_cmd.rb
+++ plugins/custom/wordcount_cmd.rb
@@ -19,16 +19,18 @@ module AresMUSH
                   scene_count = model.scenes_participated_in.size
 
                   if scene_count <1
-                    scene_count = 1
+                    msg = "This character does not have any saved scenes."
+                    client.emit_failure msg
+                  else
+                    words_per_scene = word_count / scene_count
+                    word_count = format_number(word_count)
+                    scene_count = format_number(scene_count)
+                    words_per_scene = format_number(words_per_scene)
+                    total_count = "#{model.name} has written", word_count, "words in", scene_count, "scenes for an average of", words_per_scene, "per scene."
+                    msg = total_count.join(" ")
+                    client.emit_success msg
                   end
 
-                  words_per_scene = word_count / scene_count
-                  word_count = format_number(word_count)
-                  scene_count = format_number(scene_count)
-                  words_per_scene = format_number(words_per_scene)
-                  total_count = "#{model.name} has written", word_count, "words in", scene_count, "scenes for an average of", words_per_scene, "per scene."
-                  msg = total_count.join(" ")
-                  client.emit_success msg
                 end
             end
         end
