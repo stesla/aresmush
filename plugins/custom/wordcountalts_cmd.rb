f0399c1bf7adebd7299db52c92a6c3fa2d8b7062
Clockwork EJD <4930532+ClockworkEJD@users.noreply.github.com>
2022-07-07 16:40:08 -0400

    Update wordcountalts_cmd.rb

--- plugins/custom/wordcountalts_cmd.rb
+++ plugins/custom/wordcountalts_cmd.rb
@@ -15,8 +15,8 @@ module AresMUSH
             
             def handle
 
-              alts = AresCentral.play_screen_alts(enactor)
-              alt_names = alts.all.select { |a| a.name }.map { |a| "#{a.name} - #{a.ooc_name}" }
+              alts = AresCentral.play_screen_alts(self.name)
+              alt_names = alts.select { |a| }
 
               fmt_msg = "You did a thing!", "\nAlts:", alt_names
               msg = fmt_msg.join(" ")
