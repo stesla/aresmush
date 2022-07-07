ea765690b3edb827c1bb84fd030eceb2e9973032
Clockwork EJD <4930532+ClockworkEJD@users.noreply.github.com>
2022-07-07 17:39:22 -0400

    Update wordcountalts_cmd.rb

--- plugins/custom/wordcountalts_cmd.rb
+++ plugins/custom/wordcountalts_cmd.rb
@@ -9,16 +9,20 @@ module AresMUSH
               self.name = enactor_name
             end
       
+            def alts
+              alt_list = AresCentral.alts(enactor).select { |c| }.map { |c| c.name }
+              alt_list.join(" ")
+            end
+
             def format_number(number)
               number.to_s.reverse.gsub(/(\d{3})(?=\d)/, '\\1,').reverse
             end
-            
+     
             def handle
 
-              alts = AresCentral.play_screen_alts(enactor)
               ClassTargetFinder.with_a_character(self.name, client, enactor) do |model|
 
-              fmt_msg = "#{model.name} #{model.ooc_name} You did a thing!", "\nAlts:\n"
+              fmt_msg = "#{model.ooc_name}'s word count statistics:", alt_list
               msg = fmt_msg.join(" ")
               client.emit_success msg
 
